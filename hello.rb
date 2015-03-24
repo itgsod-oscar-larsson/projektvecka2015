f1ff37dd0a0d416d4ca424ce096b1b96593240e1
itgsod-oscar-larsson <oscar.larsson@elev.it-gymnasiet.se>
2015-03-24 14:41:42 +0100

    Jag gjorde blinkande text med massa tal!

--- hello.rb
+++ hello.rb
@@ -1,3 +1,12 @@
 require 'colorize'
 
+
+$hash = Hash.new
+
+for i in 1..100
+  $hash["#{i}"] = rand(1000)
+end
+
+puts "Det här är lite nummer med saker: #{$hash.to_s}".red.blink
+
 puts "Daniel behöver hjälp".blink
